d7c3e3e7fc4d4c72ab0bfc528f239c2e86bc5458
Daniele Megna <megna.dany@gmail.com>
2017-10-07 19:08:47 +0200

    Developed some real behaviors in BirthdayService

--- src/birthday-service.js
+++ src/birthday-service.js
@@ -5,11 +5,25 @@ function BirthdayService(employeeRepository, emailService) {
 
   this.sendGreetings = function(today) {
    
-    if(employeeRepository.all().length == 2) {
-      const email = new Email('Happy birthday!', 'Happy birthday, dear Filippo!', 'filippo.depretto@github.com')
-      this.emailService.send(email)
+    const employees = employeeRepository.all()
+    if(employees.length == 0)
+      return
+
+    const employee = employees[0]
+    if(isTodayHisBirthday(employee, today)) {
+      this.emailService.send(buildBirthdayEmail(employee))
     }
     
+  }
+
+  function isTodayHisBirthday(employee, today) {
+    const employeeDateOfBirth = employee.dateOfBirth
+    return employeeDateOfBirth.month() == today.month() &&
+      employeeDateOfBirth.date() == today.date()
+  }
+
+  function buildBirthdayEmail(employee) {
+      return new Email('Happy birthday!', 'Happy birthday, dear ' + employee.firstName + '!', employee.email)
   }
   
 }
