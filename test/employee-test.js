21036e426c89cc3ec94d2338b33a86c301542f84
Daniele Megna <daniele.megna@xpeppers.com>
2017-10-12 17:46:29 +0200

    Improved Employee tests

--- test/employee-test.js
+++ test/employee-test.js
@@ -9,18 +9,27 @@ describe('Employee', () => {
     Employee()
   })
 
+  it('has some properties', () => {
+    const employee = new Employee('Daniele', 'Megna', moment('1990-09-19'), 'megna.dany@github.com')
+
+    expect(employee.firstName).to.be.equal('Daniele')
+    expect(employee.lastName).to.be.equal('Megna')
+    expect(employee.email).to.be.equal('megna.dany@github.com')
+    expect(employee.dateOfBirth).to.be.deep.equal(moment('1990-09-19'))
+  })
+
   it('is equal to another employee', () => {
     const first = new Employee('Daniele', 'Megna', moment('1990-09-19'), 'megna.dany@github.com')
     const second = new Employee('Daniele', 'Megna', moment('1990-09-19'), 'megna.dany@github.com')
 
-    expect(first).to.deep.equal(second)
+    expect(first).to.be.deep.equal(second)
   })
 
   it('is different from another employee', () => {
     const first = new Employee('Daniele', 'Megna', moment('1990-09-19'), 'megna.dany@github.com')
     const second = new Employee('Filippo', 'Verdi', moment('1990-10-07'), 'filippo.verdi@github.com')
 
-    expect(first).to.not.deep.equal(second)
+    expect(first).to.not.be.deep.equal(second)
   })
 
 })
