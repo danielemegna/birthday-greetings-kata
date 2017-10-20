e67fc23002df9bfb43bc0c863f944906c3baea71
Daniele Megna <daniele.megna@xpeppers.com>
2017-10-20 13:54:58 +0200

    Added some Email entity test

--- test/email-test.js
+++ test/email-test.js
@@ -9,15 +9,23 @@ describe('Email', () => {
     Email()
   })
 
+  it('has some public members', () => {
+    const email = new Email('Happy birthday!', 'Happy birthday, dear Daniele!', 'daniele.megna@github.com')
+
+    expect(email.subject).to.be.equal('Happy birthday!')
+    expect(email.body).to.be.equal('Happy birthday, dear Daniele!')
+    expect(email.recipient).to.be.equal('daniele.megna@github.com')
+  })
+
   it('is equal to another email', () => {
-    const first = new Email('Happy birthday!', 'Happy birthday, dear Filippo!', 'daniele.megna@github.com')
-    const second = new Email('Happy birthday!', 'Happy birthday, dear Filippo!', 'daniele.megna@github.com')
+    const first = new Email('Happy birthday!', 'Happy birthday, dear Daniele!', 'daniele.megna@github.com')
+    const second = new Email('Happy birthday!', 'Happy birthday, dear Daniele!', 'daniele.megna@github.com')
 
     expect(first).to.deep.equal(second)
   })
 
   it('is different from another email', () => {
-    const first = new Email('Happy birthday!', 'Happy birthday, dear Filippo!', 'daniele.megna@github.com')
+    const first = new Email('Happy birthday!', 'Happy birthday, dear Daniele!', 'daniele.megna@github.com')
     const second = new Email('Internal comunication', 'This is an internal comunication', 'daniele.megna@github.com')
 
     expect(first).to.not.deep.equal(second)
