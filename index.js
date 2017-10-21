18afb357f00ed7f2eb1211d1c5c42927c3aa9e5b
Daniele Megna <megna.dany@gmail.com>
2017-10-21 15:09:24 +0200

    Extracted InputFileReader module from index.js

--- index.js
+++ index.js
@@ -1,60 +1,17 @@
 const moment = require('moment')
-  Email = require('./src/entities/email')
   FSEmployeeRepository = require('./src/repositories/fs-employee-repository')
   BirthdayService = require('./src/birthday-service')
+  InputFileReader = require('./src/input-file-reader')
 
 window.processFile = function () {
-  if(!isBrowserSupported)
-    return
 
-  const inputfile = document.getElementById('inputfile')
-  if(!isFileValid(inputfile))
-    return
-
-
-  readFile(inputfile, (fileContent) => {
+  new InputFileReader(window, document, alert).read('inputfile', (fileContent) => {
     const employeeRepository = new FSEmployeeRepository(fileContent)
     const emailService = { send: (email) => alert(email) }
     const service = new BirthdayService(employeeRepository, emailService)
 
     service.sendGreetings(moment())
   })
-}
 
-function readFile(inputfile, onContentLoaded) {
-  const fileReader = new FileReader()
-  fileReader.onload = ((e) => {
-    onContentLoaded(e.target.result)
-  })
-  fileReader.readAsText(inputfile.files[0])
-}
-
-
-function isBrowserSupported() {
-  if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
-    alert('The File APIs are not fully supported in this browser.')
-    return false
-  }
-
-  return true
-}
-
-function isFileValid(inputfile) {
-  if (!inputfile) {
-    alert("Um, couldn't find the inputfile element.")
-    return false
-  }
-
-  if (!inputfile.files) {
-    alert("This browser doesn't seem to support the `files` property of file inputs.")
-    return false
-  }
-
-  if (!inputfile.files[0]) {
-    alert("Please select a file before clicking 'Load'")
-    return false
-  }
-
-  return true
 }
 
---
+++ src/input-file-reader.js
@@ -0,0 +1,58 @@
+function InputFileReader(myWindow, myDocument, outputFn) {
+
+  this.myDocument = myDocument
+  this.myWindow = myWindow
+  this.outputFn = outputFn
+
+  this.read = function(elementId, onFileReadFn) {
+    if(!isBrowserSupported)
+      return
+
+    const inputfile = this.myDocument.getElementById('inputfile')
+    if(!isFileValid(inputfile))
+      return
+
+
+    readFile(inputfile, onFileReadFn)
+  }
+
+  function readFile(inputfile, onContentLoaded) {
+    const fileReader = new FileReader()
+    fileReader.onload = ((e) => {
+      onContentLoaded(e.target.result)
+    })
+    fileReader.readAsText(inputfile.files[0])
+  }
+
+
+  function isBrowserSupported() {
+    if (!myWindow.File || !myWindow.FileReader || !myWindow.FileList || !myWindow.Blob) {
+      outputFn('The File APIs are not fully supported in this browser.')
+      return false
+    }
+
+    return true
+  }
+
+  function isFileValid(inputfile) {
+    if (!inputfile) {
+      outputFn("Um, couldn't find the inputfile element.")
+      return false
+    }
+
+    if (!inputfile.files) {
+      outputFn("This browser doesn't seem to support the `files` property of file inputs.")
+      return false
+    }
+
+    if (!inputfile.files[0]) {
+      outputFn("Please select a file before clicking 'Load'")
+      return false
+    }
+
+    return true
+  }
+
+}
+
+module.exports = InputFileReader
